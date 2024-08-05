1fd4884b8f87fa7810f197bda7081e2ad7cae6ed
Karina <psinka.94@gmail.com>
2024-08-05 13:43:16 +0300

    Add files via upload

---
+++ Module5Homework3.py
@@ -0,0 +1,84 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return False
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+
+House1 = House('ЖК Лесной', 5)
+House2 = House('Столица', 25)
+
+print(House1)
+print(House2)
+
+print(House1 == House2)
+
+House1 = House1 + 20
+print(House1)
+
+print(House1 == House2)
+
+House1 += 5
+print(House1)
+House2 = 5 + House2
+print(House2)
+
+print(House1 > House2)
+print(House1 >= House2)
+print(House1 < House2)
+print(House1 <= House2)
+print(House1 != House2)
